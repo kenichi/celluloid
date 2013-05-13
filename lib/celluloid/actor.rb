5f985999c18b617c21803fa2fc52716e8b12d358
Tim Carey-Smith <tim@spork.in>
2013-05-14 09:37:08 +1200

    Remove deprecated finalizer code

--- lib/celluloid/actor.rb
+++ lib/celluloid/actor.rb
@@ -380,14 +380,6 @@ module Celluloid
 
     # Run the user-defined finalizer, if one is set
     def run_finalizer
-      # FIXME: remove before Celluloid 1.0
-      if @subject.respond_to?(:finalize) && @subject.class.finalizer != :finalize
-        Logger.warn("DEPRECATION WARNING: #{@subject.class}#finalize is deprecated and will be removed in Celluloid 1.0. " +
-          "Define finalizers with '#{@subject.class}.finalizer :callback.'")
-
-        task(:finalizer, :finalize) { @subject.finalize }
-      end
-
       finalizer = @subject.class.finalizer
       if finalizer && @subject.respond_to?(finalizer, true)
         task(:finalizer, :finalize) { @subject.__send__(finalizer) }
